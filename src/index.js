05df9fa245288e3b80820bd63649a40312adc29e
Emily Znamierowski <emilyannz@gmail.com>
2016-03-28 21:08:08 -0500

    markup and styling almost complete

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@
 import ComicsListView from 'comics-list-view';
 import SeriesInfoView from 'series-info-view';
 import CharacterListView from 'character-list-view';
-const id = 12916;
+const id = #####;
+
+export default function () {
+  fetch(`http://gateway.marvel.com:80/v1/public/series/9856?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  .then((response) => response.json())
+  .then((info) => {
+    // Select profile container
+    const element = document.querySelector(`.sidebar`);
+    // New profile data
+    const profile = new SeriesInfoView(element, info.data.results[0]);
+  });
+
+  fetch(`http://gateway.marvel.com:80/v1/public/series/9856/characters?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  .then((response) => response.json())
+  .then((info) => {
+    const element = document.querySelector(`.characters`);
+    const character = new CharacterListView(element, info.data.results);
+  });
+
+  fetch(`http://gateway.marvel.com:80/v1/public/series/9856/comics?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  .then((response) => response.json())
+  .then((info) => {
+    const element = document.querySelector(`.comics`);
+    const comic = new ComicListView(element, info.data.results);
+  });
+}
