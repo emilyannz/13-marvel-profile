4ab04b8b01de291a34097d8467ae0aa9fcd7297e
Emily Znamierowski <emilyannz@gmail.com>
2016-04-21 21:24:26 -0500

    Completed src index module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 'use strict';
 // import SeriesInfoView from 'series-info-view';
-import setSeries from 'setSeries';
+import SidebarView from 'setSeries';
 import CharacterListView from 'character-list-view';
+import ComicView from 'comic-view';
 import ComicListView from 'comic-list-view';
-const id = `20448`;
 
 
-export default function () {
-  setSeries();
+
+export default function() {
+  const id = 20448;
+
   // .then((response) => response.json())
   // .then((info) => {
   //   // Select profile container
@@ -16,17 +18,26 @@ export default function () {
   //   const profile = new SeriesInfoView(element, info.data.results[0]);
   // });
 
-  // fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=03d447ae0f2abf63a91a516b765da6e5`)
-  // .then((response) => response.json())
-  // .then((info) => {
-  //   const element = document.querySelector(`.characters`);
-  //   const character = new CharacterListView(element, info.data.results);
-  // });
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+    .then((response) => response.json())
+    .then((data) => {
+      const series = data.data.results[0];
+      const sidebar = document.querySelector(`.sidebar`);
+      const seriesView = new SidebarView(sidebar, series);
+    });
 
   fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=03d447ae0f2abf63a91a516b765da6e5`)
-  .then((response) => response.json())
-  .then((info) => {
-    const element = document.querySelector(`.comics`);
-    const comic = new ComicListView(element, info.data.results);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      const characters = data.data.results;
+      const characterElement = document.querySelector(`.characters-list`);
+      const characterView = new CharacterListView(characterElement, characters);
+    });
+
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+    .then((response) => response.json())
+    .then((data) => {
+      const comics = data.data.results;
+      const comicElement = new ComicListView(comicElement, comics);
+    })
 }
