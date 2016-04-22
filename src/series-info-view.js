2ae1bd9f2e43e2f1a15cc1ed8304f5b31db9abc7
Emily Znamierowski <emilyannz@gmail.com>
2016-04-22 00:55:12 -0500

    Completed series-info-view.js

--- src/series-info-view.js
+++ src/series-info-view.js
@@ -1,35 +1,62 @@
 'use strict';
 
-export default class SeriesInfoView {
+export default class SidebarView {
   constructor(element, data) {
     this.element = element;
     this.data = data;
+    this.creatorList();
 
-    this.renderImage();
-    this.renderTitle();
-    this.renderYears();
-    this.renderCreators();
+    const imageUrl = `${data.thumbnail.path}.${data.thumbnail.extension}`;
+
+    // this.element.querySelector(`.img-container__pic`).src = imgUrl;
+
+    this.element.querySelector(`.sidebar-profile__name`).innerText = data.title;
+
+    this.element.querySelector(`.sidebar-profile__year`).innerText =
+     `${data.startYear} - ${data.endYear}`;
   }
 
-  renderImage() {
-    this.element.querySelector(`.profile-pic`).innerHTML = `
-      <img class="profile-pic--image" src="${this.data.thumbnail.path}.${this.data.thumbnail.extension}" alt=""/>
-    `;
-  }
-
-  renderTitle() {
-    this.element.querySelector(`.profile-title`).innerText = this.data.title;
-  }
-
-  renderYears() {
-    this.element.querySelector(`.profile-years`).innerText = `${this.data.startYear} - ${this.data.endYear}`;
-  }
-
-  renderCreators() {
+  creatorList() {
     this.data.creators.items.forEach((creator) => {
-      const newListItem = document.createElement(`li`);
-      newListItem.innerText = creator.name;
-      this.element.querySelector(`.creator-list`).appendChild(newListItem);
+      const creatorsListItems = document.querySelector(`.creators`);
+      const creatorsElement = document.createElement(`li`);
+      creatorsElement.innerText = creator.name;
+      creatorsListItems.appendChild(creatorsElement);
     });
   }
 }
+
+
+
+
+
+
+
+//     this.renderImage();
+//     this.renderTitle();
+//     this.renderYears();
+//     this.renderCreators();
+//   }
+//
+//   renderImage() {
+//     this.element.querySelector(`.profile-pic`).innerHTML = `
+//       <img class="profile-pic--image" src="${this.data.thumbnail.path}.${this.data.thumbnail.extension}" alt=""/>
+//     `;
+//   }
+//
+//   renderTitle() {
+//     this.element.querySelector(`.profile-title`).innerText = this.data.title;
+//   }
+//
+//   renderYears() {
+//     this.element.querySelector(`.profile-years`).innerText = `${this.data.startYear} - ${this.data.endYear}`;
+//   }
+//
+//   renderCreators() {
+//     this.data.creators.items.forEach((creator) => {
+//       const newListItem = document.createElement(`li`);
+//       newListItem.innerText = creator.name;
+//       this.element.querySelector(`.creator-list`).appendChild(newListItem);
+//     });
+//   }
+// }
