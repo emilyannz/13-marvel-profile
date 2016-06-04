d556dbe7a38e1254022046d1f7174d389b6d1725
Emily Znamierowski <emilyannz@gmail.com>
2016-06-03 19:22:48 -0500

    Overhauled setSeries

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 'use strict';
 
-import SidebarView from 'series-info-view';
-import CharacterView from 'character-view';
+import setSeries from 'setSeries';
 import CharacterListView from 'character-list-view';
-import ComicView from 'comic-view';
 import ComicListView from 'comic-list-view';
+const id = 20448;
 
 export default function () {
-  const id = 20448;
-
-  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=03d447ae0f2abf63a91a516b765da6e5`)
-    .then((response) => response.json())
-    .then((data) => {
-      const series = data.data.results[0];
-      const sidebar = document.querySelector(`.sidebar`);
-      const seriesView = new SidebarView(sidebar, series);
-    });
+  setSeries();
+  // fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     const series = data.data.results[0];
+  //     const sidebar = document.querySelector(`.sidebar`);
+  //     const seriesView = new SidebarView(sidebar, series);
+  //   });
+  //
+  // fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     const characters = data.data.results;
+  //     const characterElement = document.querySelector(`.characters-container`);
+  //     const characterView = new CharacterListView(characterElement, characters);
+  //   });
 
   fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=03d447ae0f2abf63a91a516b765da6e5`)
     .then((response) => response.json())
-    .then((data) => {
-      const characters = data.data.results;
-      const characterElement = document.querySelector(`.characters-container`);
-      const characterView = new CharacterListView(characterElement, characters);
-    });
-
-  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/comics?apikey=03d447ae0f2abf63a91a516b765da6e5`)
-    .then((response) => response.json())
-    .then((data) => {
-      const comics = data.data.results;
-      const comicElement = document.querySelector(`.comics-container`);
-      const comicView = new ComicListView(comicElement, comics);
-    });
+    .then((info) => {
+      const element = document.querySelector(`.comics`);
+      const comic = new ComicListView(element, info.data.results);
+  });
 }
--- src/series-info-view.js
+++ src/series-info-view.js
@@ -1,24 +1,22 @@
+
 'use strict';
+import SeriesInfoView from 'series-info-view';
+import CharacterListView from 'character-list-view';
 
-export default class SidebarView {
-  constructor(element, data) {
-    this.element = element;
-    this.data = data;
-    this.creatorList();
+export default function (id = `9856`) {
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  .then((response) => response.json())
+  .then((info) => {
+    // Select profile container
+    const element = document.querySelector(`.sidebar`);
+    // New profile data
+    const profile = new SeriesInfoView(element, info.data.results[0]);
+  });
 
-    const imageUrl = `${data.thumbnail.path}.${data.thumbnail.extension}`;
-    this.element.querySelector(`.sidebar-image-container__picture`).src = imageUrl;
-    this.element.querySelector(`.sidebar-profile__name`).innerText = data.title;
-    this.element.querySelector(`.sidebar-profile__year`).innerText =
-     `${data.startYear} - ${data.endYear}`;
-  }
-
-  creatorList() {
-    this.data.creators.items.forEach((creator) => {
-      const creatorsListItems = document.querySelector(`.creators`);
-      const creatorsElement = document.createElement(`li`);
-      creatorsElement.innerText = creator.name;
-      creatorsListItems.appendChild(creatorsElement);
-    });
-  }
+  fetch(`http://gateway.marvel.com:80/v1/public/series/${id}/characters?apikey=03d447ae0f2abf63a91a516b765da6e5`)
+  .then((response) => response.json())
+  .then((info) => {
+    const element = document.querySelector(`.characters`);
+    const character = new CharacterListView(element, info.data.results);
+  });
 }
